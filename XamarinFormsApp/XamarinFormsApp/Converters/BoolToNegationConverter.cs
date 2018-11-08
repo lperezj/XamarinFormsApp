c99f5457f7711806e6b1b2d4ad69d6c6d026ce56
Luis Perez Jimenez <luis.perez.jimenez@avanade.com>
2018-11-08 16:46:37 +0100

    Converter

---
+++ XamarinFormsApp/XamarinFormsApp/Converters/BoolToNegationConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace XamarinFormsApp.Converters
+{
+    using System;
+    using System.Globalization;
+
+    using Xamarin.Forms;
+
+    public class BoolToNegationConverter : IValueConverter
+    {
+        #region Public Methods
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(value is bool && (bool)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(value is bool && (bool)value);
+        }
+
+        #endregion
+    }
+}
